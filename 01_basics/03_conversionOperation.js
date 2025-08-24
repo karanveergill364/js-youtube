ee6fea3e0bb9f1f32f57dbb67b1f9337e778ed73
karanveergill364 <156914156+karanveergill364@users.noreply.github.com>
2025-08-24 07:22:43 +0000

    conversion operations comp

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33abc"
+
+let scoreInnumber = Number(score)
+console.log(typeof scoreInnumber)
+console.log(scoreInnumber)  //its a NaN
+
+//conversion into string 
+let number = 33
+let numString = String(number)
+console.log(typeof numString)
+
+// boolean conversion
+
+let isloogedIn = true
+let num = Number(isloogedIn)
+console.log(num)
+
+// true=>1
+//false=>0
+//alphabets=>NaN
