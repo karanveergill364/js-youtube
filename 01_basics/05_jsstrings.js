e014ae209b80900bd8903822975b93ffabcf9577
karanveergill364 <156914156+karanveergill364@users.noreply.github.com>
2025-08-24 07:21:43 +0000

    js strings

---
+++ 01_basics/05_jsstrings.js
@@ -0,0 +1,12 @@
+const name = "karan"
+
+console.log(`Hello my name is ${name}`)
+
+const gameName = new String('karanveer')
+
+console.log(gameName[1])
+
+console.log(gameName.charAt(2))
+
+console.log()
+const name2 = ""
