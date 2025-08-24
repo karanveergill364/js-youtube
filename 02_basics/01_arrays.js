6956c7820b4e567dbd334889c9c5ebbd95da610f
karanveergill364 <156914156+karanveergill364@users.noreply.github.com>
2025-08-24 08:00:12 +0000

    ls Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,15 @@
+const myArr = [1,2,3,"karan"]
+
+console.log(myArr);
+
+console.log(typeof(myArr.at(3)))
+myArr.push(666666666)
+console.log(myArr)
+
+const arr1 = new Array(54, 4, 3, 6, 3)
+const arr2 = new Array(8, 4, 7, 2, 5)
+
+console.log(arr1)
+
+console.log(Array.of(arr1))
+
